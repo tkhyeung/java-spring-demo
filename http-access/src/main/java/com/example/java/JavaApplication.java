118119f964d60beaefcca49f19c4fd35d356a320
hkinnkun <hkinnkun@gmail.com>
2021-07-17 13:50:28 +0800

    add OpenFeign to http-access

--- http-access/src/main/java/com/example/java/JavaApplication.java
+++ http-access/src/main/java/com/example/java/JavaApplication.java
@@ -1,13 +1,18 @@
 package com.example.java;
 
 import com.example.java.service.RestTemplateService;
+import com.example.java.service.TestFeignService;
 import com.example.java.service.WebClientService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@Slf4j
 @SpringBootApplication
+@EnableFeignClients
 public class JavaApplication implements CommandLineRunner {
 
     @Autowired
@@ -15,6 +20,9 @@ public class JavaApplication implements CommandLineRunner {
 
     @Autowired
     private WebClientService webClientService;
+
+    @Autowired
+    private TestFeignService feignService;
 
     public static final String RESOURCE_URL = "https://jsonplaceholder.typicode.com";
 
@@ -24,7 +32,9 @@ public class JavaApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        restTemplateService.test();
-        webClientService.test();
+        log.info("Start http-access test");
+//        restTemplateService.test();
+//        webClientService.test();
+        feignService.test();
     }
 }
