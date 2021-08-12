fa70968814caae02cd8e1c1162552bb5c1fa9512
hkinnkun <hkinnkun@gmail.com>
2021-08-13 02:27:53 +0800

    add atomic variable & synchronized examples

--- spring-async-future/src/main/java/com/example/java/JavaApplication.java
+++ spring-async-future/src/main/java/com/example/java/JavaApplication.java
@@ -1,6 +1,8 @@
 package com.example.java;
 
 import com.example.java.service.AsyncService;
+import com.example.java.service.AsyncServiceUsingAtomicVariable;
+import com.example.java.service.AsyncServiceUsingSynchronized;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -22,6 +24,14 @@ public class JavaApplication implements CommandLineRunner {
     @Autowired
     private AsyncService service;
 
+    @Autowired
+    private AsyncServiceUsingAtomicVariable serviceUsingAtomicVariable;
+
+    @Autowired
+    private AsyncServiceUsingSynchronized serviceUsingSynchronized;
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(JavaApplication.class, args);
     }
@@ -29,15 +39,16 @@ public class JavaApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        test();
-        log.info("--------------------------------");
-        test();
-        log.info("--------------------------------");
-        test();
-        log.info("--------------------------------");
-        test();
-        log.info("--------------------------------");
-        test();
+//        test();
+//        log.info("--------------------------------");
+//        test();
+//        log.info("--------------------------------");
+//        test();
+//        log.info("--------------------------------");
+//        test();
+//        log.info("--------------------------------");
+//        test();
+        testAtmoic();
     }
 
     private void test() {
@@ -54,6 +65,46 @@ public class JavaApplication implements CommandLineRunner {
         log.info("6");
         log.info(result.toString());
         log.info("7");
+
+    }
+
+    private void testAtmoic(){
+        log.info("test Atomic::");
+
+
+        Stream.of(
+                serviceUsingAtomicVariable.add(4,1),
+                serviceUsingAtomicVariable.add(3,2),
+                serviceUsingAtomicVariable.add(5,3),
+                serviceUsingAtomicVariable.minus(7,4),
+                serviceUsingAtomicVariable.minus(1,5)
+        ).map(CompletableFuture::join).collect(Collectors.toList());
+
+        log.info("count is {}", serviceUsingAtomicVariable.getValue());
+
+
+        // atomic variable uses compare and swap algorithm, non block, optimistic locking;
+        // Atomic variable internally does not use lock, henus no thread suspension and resumption
+        // synchronised uses locks, costly
+        // should use synchronised to all accessible methods
+
+        // https://www.geeksforgeeks.org/difference-between-atomic-volatile-and-synchronized-in-java/
+        //Instance methods
+        //Static methods
+        //Code blocks (code blocks with in a method)
+
+        //todo reentrant lock
+        //https://stackoverflow.com/questions/11821801/why-use-a-reentrantlock-if-one-can-use-synchronizedthis
+
+//        Stream.of(
+//                serviceUsingSynchronized.add(4,1),
+//                serviceUsingSynchronized.add(3,2),
+//                serviceUsingSynchronized.add(5,3),
+//                serviceUsingSynchronized.minus(7,4),
+//                serviceUsingSynchronized.minus(1,5)
+//        ).map(CompletableFuture::join).collect(Collectors.toList());
+//
+//        log.info("count is {}", serviceUsingSynchronized.getValue());
 
     }
 }
--- spring-async-future/src/main/java/com/example/java/service/AsyncService.java
+++ spring-async-future/src/main/java/com/example/java/service/AsyncService.java
@@ -5,6 +5,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @Service
@@ -35,5 +36,38 @@ public class AsyncService {
         log.info("Start test4");
         return CompletableFuture.completedFuture(400);
     }
+
+    public volatile int counter = 0;
+
+    @Async
+    public synchronized CompletableFuture<Void> asyncIncrementWithSynchronized(int id) {
+        return CompletableFuture.runAsync(() -> {
+            log.info("before Increment::id:{}::counter::{}",id,counter);
+            int localCounter = counter;
+            localCounter++;
+            counter = localCounter;
+            log.info("after Increment::id:{}::counter::{}",id,counter);
+        });
+    }
+
+    public final AtomicInteger atomicCounter = new AtomicInteger(0);
+
+    public int getValue() {
+        return atomicCounter.get();
+    }
+    @Async
+    public CompletableFuture<Void> asyncIncrementWithAtomicInteger(int id) {
+        return CompletableFuture.runAsync(() -> {
+            int existingValue = getValue();
+            log.info("before asyncIncrementWithAtomicInteger::id:{}::counter::{}",id,existingValue);
+            int newValue = existingValue + 1;
+            do{
+
+            }while(atomicCounter.compareAndSet(existingValue, newValue));
+            log.info("after asyncIncrementWithAtomicInteger::id:{}::counter::{}",id,newValue);
+        });
+    }
+
+
 
 }
