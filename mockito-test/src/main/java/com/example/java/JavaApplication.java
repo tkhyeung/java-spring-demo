6286d11911f0cc6d260a7f9326e768282019bf9f
hkinnkun <hkinnkun@gmail.com>
2021-12-22 00:26:25 +0800

    add mockito mocking static methods

--- mockito-test/src/main/java/com/example/java/JavaApplication.java
+++ mockito-test/src/main/java/com/example/java/JavaApplication.java
@@ -4,6 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@Mock to create a mock
+//@Spy is on real instance
+//MockStatic can mock a static method
+//ArgumentCaptor can capture the value passed onto the Mock
 @Slf4j
 @SpringBootApplication
 public class JavaApplication {
--- mockito-test/src/test/java/com/example/java/TodoValidatorTest.java
+++ mockito-test/src/test/java/com/example/java/TodoValidatorTest.java
@@ -1,10 +1,13 @@
 package com.example.java;
 
 import lombok.val;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -38,6 +41,44 @@ public class TodoValidatorTest {
         //then
 //        verify(validator, times(1)).validate(id);
     }
+
+    @Test
+    void testShouldReturnThrowsErrorWithMockingStaticMethod() {
+        //given
+        String id = "abc";
+        MockedStatic<StringUtils> stringUtilsMockedStatic = Mockito.mockStatic(StringUtils.class);
+        stringUtilsMockedStatic.when(() ->
+            StringUtils.isNumeric(id)
+        ).thenReturn(false);
+        //when
+//        doThrow(new IllegalArgumentException()).when(validator).validate(eq(id));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> validator.validate(id));
+        //then
+//        verify(validator, times(1)).validate(id);
+        stringUtilsMockedStatic.verify(() -> StringUtils.isNumeric(id));
+        stringUtilsMockedStatic.close();
+    }
+
+    @Test
+    void testShouldReturnNothingWhenValidateIsOkWithMockingStaticMethod() {
+        //given
+        String id = "1";
+        MockedStatic<StringUtils> stringUtilsMockedStatic = Mockito.mockStatic(StringUtils.class);
+        stringUtilsMockedStatic.when(() ->
+                StringUtils.isNumeric(id)
+        ).thenReturn(true);
+
+        //when
+//        doNothing().when(validator).validate(eq(id));
+        validator.validate(id);
+        //then
+//        verify(validator, times(1)).validate(id);
+        stringUtilsMockedStatic.verify(() -> StringUtils.isNumeric(id));
+        stringUtilsMockedStatic.close();
+
+    }
+
+
 
 
 
